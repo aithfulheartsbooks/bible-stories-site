7cd2d6543741f4b2142a443bb32df6ca530e7acd
aithfulheartsbooks <info@faithfulheartsbooks.com>
2026-05-09 17:10:18 -0700

    Redirect root domain to www

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "faithfulheartsbooks.com",
+          },
+        ],
+        destination: "https://www.faithfulheartsbooks.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
